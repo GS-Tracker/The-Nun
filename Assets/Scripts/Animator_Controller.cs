20ed5aae0795a95bf6a4409e8147d1817743dda1
GS-Tracker <pola.awny.92@gmail.com>
2019-03-09 16:10:27 +0200

    Forest initialized with nav mesh surface applied upon it and the horse model has the ability to path_find to reach the abbey gate

---
+++ Assets/Scripts/Animator_Controller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animator_Controller : MonoBehaviour
+{
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void horseTrot()
+    {
+        anim.Play("Armature|Trot Cycle");
+    }
+
+    public void horseWalk()
+    {
+        anim.Play("Armature|Walk Cycle");
+    }
+
+    public void horseStandIdle()
+    {
+        anim.Play("Idle");
+    }
+
+}
---
+++ Assets/Scripts/Horse_Move.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class Horse_Move : MonoBehaviour
+{
+    public Transform targetDestination;
+    public GameObject myHorse_Model;
+    Animator_Controller my_horseModel_Script;
+
+    NavMeshAgent agent;
+
+    float horseSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("The horse is ready to move");
+        agent = this.GetComponent<NavMeshAgent>();
+
+        horseSpeed = agent.speed;
+        my_horseModel_Script = myHorse_Model.GetComponent<Animator_Controller>();
+
+        agent.SetDestination(targetDestination.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (agent.remainingDistance == 0 && agent.remainingDistance != Mathf.Infinity && agent.pathStatus == NavMeshPathStatus.PathComplete)
+        {
+            my_horseModel_Script.horseStandIdle();
+        } else
+        {
+            
+            Debug.Log(agent.remainingDistance);
+
+            // If the remaining distance isn't big enough, slow the movement of the horse.
+            if (agent.remainingDistance != Mathf.Infinity)
+            {
+                agent.speed = 5;
+                my_horseModel_Script.horseWalk();
+            }
+            else
+            {
+                my_horseModel_Script.horseTrot();
+            }
+        } 
+    }
+
+}
